5d6d0521d1a1c98451e9672a4a0bae5ae40556aa
yincade <101445104+yincade@users.noreply.github.com>
2024-05-15 16:00:43 -0400

    added clickability to korean word in hanja example

--- frontend/vite_kordict/src/components/panel/paginated_results/HanjaExampleResult.jsx
+++ frontend/vite_kordict/src/components/panel/paginated_results/HanjaExampleResult.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import { ViewContext } from "../Panel";
 import StringWithHanja from "../StringWithHanja";
 import "./styles/hanja-example-result-styles.css";
+import "../universal-styles.css";
 
 const HanjaExampleResult = ({ result }) => {
+  const viewContext = useContext(ViewContext);
+
+  const viewKoreanDetail = (targetCode) => {
+    viewContext["updateViewAndPushToHistory"]({
+      view: "detail_korean",
+      value: targetCode,
+      searchBarInitialState: {
+        boxContent: result["kw_word"],
+        dictionary: "korean",
+      },
+    });
+  };
+
   return (
     <div>
       <div className="hanja_example_word">
-        {result["kw_word"]}
+        <span
+          className="clickable-result"
+          onClick={() => {
+            viewKoreanDetail(result["kw_target_code"]);
+          }}
+        >
+          {result["kw_word"]}
+        </span>
         (
         <StringWithHanja string={result["kw_origin"]} />)
       </div>
