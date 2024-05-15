b50a833657b6b8c94a99d497eec691f1da02d20c
yincade <101445104+yincade@users.noreply.github.com>
2024-05-14 21:53:16 -0400

    added structure for homepage

--- frontend/vite_kordict/src/components/panel/home/HomePage.jsx
+++ frontend/vite_kordict/src/components/panel/home/HomePage.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useAPIFetcher } from "../useAPIFetcher";
+import { ViewContext } from "../Panel.jsx";
+import { LoadingMessage } from "../../LoadingMessage";
 
 const HomePage = () => {
-  return <div>홈페이지</div>;
+  const [homepageData, setHomepageData] = useState();
+  const { apiFetch, loading, error } = useAPIFetcher();
+
+  useEffect(() => {
+    apiFetch("http://127.0.0.1:8000/api/homepage_info/", setHomepageData);
+  }, []);
+
+  return (
+    <React.Fragment>
+      {localStorage.getItem("username") == null ? (
+        <div className="logged-out-homepage">
+          로그인 후 개인에 맞는 홈페이가 볼 수 있습니다.
+        </div>
+      ) : (
+        <div className="logged-in-homepage">
+          {loading ? <LoadingMessage /> : <div></div>}
+        </div>
+      )}
+    </React.Fragment>
+  );
 };
 
 export default HomePage;
