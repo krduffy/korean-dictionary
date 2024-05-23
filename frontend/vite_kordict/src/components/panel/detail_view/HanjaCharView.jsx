577ef4c2027139b83886c61d6c8ed16230cdc0d7
yincade <101445104+yincade@users.noreply.github.com>
2024-05-22 20:55:26 -0400

    feature: reformatted hanja detail view
    added new data from new dataset

--- frontend/vite_kordict/src/components/panel/detail_view/HanjaCharView.jsx
+++ frontend/vite_kordict/src/components/panel/detail_view/HanjaCharView.jsx
@@ -8,6 +8,8 @@ import HanjaWriter from "../hanja-writing/HanjaWriter.jsx";
 import { LoadingMessage } from "../messages/LoadingMessage.jsx";
 import PaginatedResults from "../paginated_results/PaginatedResults.jsx";
 import ClipboardCopier from "../string_formatters/ClipboardCopier.jsx";
+import StringWithHanja from "../string_formatters/StringWithHanja.jsx";
+import StringWithNLP from "../string_formatters/StringWithNLP.jsx";
 
 import "./styles/hanja-char-view-styles.css";
 
@@ -28,18 +30,142 @@ const HanjaCharView = ({ hanjaChar }) => {
                 <LoadingMessage />
             ) : (
                 <div>
-                    <HanjaWriter character={hanjaChar} />
-                    <div className="jahuneum">
-                        <span className="hanja-header">
-                            {hanjaChar}
-                            <ClipboardCopier string={charData["character"]} />
-                        </span>{" "}
-                        <span className="meaning-reading-header">
-                            {charData["meaning_reading"]
-                                ? charData["meaning_reading"]
-                                : "훈음 정보가 없습니다."}
-                        </span>
+                    <div className="main-info-section">
+                        <div className="main-info-upper">
+                            <div className="main-info-upper-left">
+                                <div className="jahuneum">
+                                    <span className="hanja-header">
+                                        {hanjaChar}
+                                    </span>{" "}
+                                    <span className="meaning-reading-header">
+                                        {charData["meaning_reading"]
+                                            ? charData["meaning_reading"]
+                                            : "훈음 정보가 없습니다."}
+                                    </span>
+                                </div>
+                                <div className="main-info-tables">
+                                    <table
+                                        className="main-info-table"
+                                        id="main-info-table-left"
+                                    >
+                                        <tbody>
+                                            <tr className="main-info-table-row">
+                                                <th className="main-info-table-head">
+                                                    획수
+                                                </th>
+                                                <td className="main-info-table-data">
+                                                    {charData["strokes"]
+                                                        ? charData["strokes"] +
+                                                          "획"
+                                                        : "-"}
+                                                </td>
+                                            </tr>
+
+                                            <tr className="main-info-table-row">
+                                                <th className="main-info-table-head">
+                                                    부수
+                                                </th>
+                                                <td className="main-info-table-data">
+                                                    {charData["radical"] ? (
+                                                        <StringWithHanja
+                                                            string={
+                                                                charData[
+                                                                    "radical"
+                                                                ]
+                                                            }
+                                                        />
+                                                    ) : (
+                                                        "-"
+                                                    )}
+                                                </td>
+                                            </tr>
+
+                                            <tr className="main-info-table-row">
+                                                <th className="main-info-table-head">
+                                                    모양자 분해
+                                                </th>
+                                                <td className="main-info-table-data">
+                                                    {charData[
+                                                        "decomposition"
+                                                    ] ? (
+                                                        <StringWithHanja
+                                                            string={
+                                                                charData[
+                                                                    "decomposition"
+                                                                ]
+                                                            }
+                                                        />
+                                                    ) : (
+                                                        "-"
+                                                    )}
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <table
+                                        className="main-info-table"
+                                        id="main-info-table-right"
+                                    >
+                                        <tbody>
+                                            <tr className="main-info-table-row">
+                                                <th className="main-info-table-head">
+                                                    급수별
+                                                </th>
+                                                <td className="main-info-table-data">
+                                                    {charData["exam_level"]
+                                                        ? charData["exam_level"]
+                                                        : "-"}
+                                                </td>
+                                            </tr>
+
+                                            <tr className="main-info-table-row">
+                                                <th className="main-info-table-head">
+                                                    교육용
+                                                </th>
+                                                <td className="main-info-table-data">
+                                                    {charData["grade_level"]
+                                                        ? charData[
+                                                              "grade_level"
+                                                          ]
+                                                        : "-"}
+                                                </td>
+                                            </tr>
+
+                                            <tr className="main-info-table-row">
+                                                <th className="main-info-table-head">
+                                                    유니코드
+                                                </th>
+                                                <td className="main-info-table-data">
+                                                    U+
+                                                    {hanjaChar
+                                                        .charCodeAt(0)
+                                                        .toString(16)
+                                                        .toUpperCase()}
+                                                </td>
+                                                <td>
+                                                    <ClipboardCopier
+                                                        string={hanjaChar}
+                                                    />
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div className="main-info-upper-right">
+                                <HanjaWriter character={hanjaChar} />
+                            </div>
+                        </div>
+
+                        {charData["explanation"] && (
+                            <div className="main-info-lower">
+                                <StringWithNLP
+                                    string={charData["explanation"]}
+                                />
+                            </div>
+                        )}
                     </div>
+
                     <div className="additional-info-section-header">
                         연관단어
                     </div>
