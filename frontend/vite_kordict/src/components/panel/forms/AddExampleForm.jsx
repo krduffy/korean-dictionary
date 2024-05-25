65e7f56342a3eb991160805746c4c1ccf7fe39da
yincade <101445104+yincade@users.noreply.github.com>
2024-05-25 14:45:01 -0400

    moved add example form

---
+++ frontend/vite_kordict/src/components/panel/forms/AddExampleForm.jsx
@@ -0,0 +1,140 @@
+import React, { useContext, useEffect, useState } from "react";
+
+import { useAPIModifier } from "../../../hooks/useAPIModifier.js";
+
+import { AuthenticationInfoContext } from "../../../App.jsx";
+
+const AddExampleForm = ({ wordTargetCode, initiallyExistingExamples }) => {
+    const authInfo = useContext(AuthenticationInfoContext)["authInfo"];
+
+    // Log the initiallyExistingExamples to verify the prop being passed
+    console.log("initiallyExistingExamples:", initiallyExistingExamples);
+
+    // Use a function to initialize state for better handling of initial values
+    const [exampleList, setExampleList] = useState(initiallyExistingExamples);
+    useEffect(() => {
+        setExampleList(initiallyExistingExamples);
+    }, [initiallyExistingExamples]);
+
+    const {
+        formData,
+        updateFormDataField,
+        apiModify,
+        successful,
+        response,
+        error,
+        loading,
+    } = useAPIModifier({
+        referent: wordTargetCode,
+        definition: "정의", // will not be rendered in sense view component because order is 0
+        additional_info: {
+            example_info: exampleList,
+        },
+        order: 0,
+    });
+
+    const updateExampleInfo = (index, field, value) => {
+        const newExampleList = [...exampleList];
+        newExampleList[index] = {
+            ...newExampleList[index],
+            [field]: value,
+        };
+        setExampleList(newExampleList);
+        updateFormDataField("additional_info", {
+            example_info: newExampleList,
+        });
+    };
+
+    useEffect(() => {
+        console.log("example list is now");
+        console.log(exampleList);
+    }, [exampleList]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const method = initiallyExistingExamples ? "PUT" : "POST";
+
+        apiModify(
+            "http://127.0.0.1:8000/api/create_sense/",
+            authInfo["token"],
+            formData,
+            method
+        );
+    };
+
+    const addNewExample = () => {
+        setExampleList([
+            ...exampleList,
+            {
+                example: "",
+                source: "",
+            },
+        ]);
+    };
+
+    return (
+        <div className="add-example-form">
+            <div className="add-example-form-upper-bar">
+                <div className="add-example-header">예문 추가</div>
+            </div>
+
+            {exampleList.map((example, index) => (
+                <div className="text-area-container" key={index}>
+                    <dl>
+                        <dt className="text-area-container-dt">예문 (필수)</dt>
+                        <dd className="text-area-container-dd">
+                            <textarea
+                                className="add-example-text-area"
+                                value={example.example}
+                                onChange={(event) =>
+                                    updateExampleInfo(
+                                        index,
+                                        "example",
+                                        event.target.value
+                                    )
+                                }
+                            ></textarea>
+                        </dd>
+                    </dl>
+                    <dl>
+                        <dt className="text-area-container-dt">출처 (선택)</dt>
+                        <dd className="text-area-container-dd">
+                            <textarea
+                                className="add-example-text-area"
+                                value={example.source}
+                                onChange={(event) =>
+                                    updateExampleInfo(
+                                        index,
+                                        "source",
+                                        event.target.value
+                                    )
+                                }
+                            ></textarea>
+                        </dd>
+                    </dl>
+                </div>
+            ))}
+
+            <div className="add-example-form-lower-bar">
+                <div className="add-example-tip">
+                    예문 입력 시 중활고에 담는 단어 아래에 밑줄을 그려줍니다.
+                </div>
+                <button
+                    onClick={(e) => handleSubmit(e)}
+                    className="add-example-submit-button"
+                >
+                    추가
+                </button>
+                <button
+                    onClick={addNewExample}
+                    className="add-new-example-button"
+                >
+                    예문 추가
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default AddExampleForm;
--- frontend/vite_kordict/src/components/panel/forms/EditWordForm.jsx
+++ frontend/vite_kordict/src/components/panel/forms/EditWordForm.jsx
@@ -7,19 +7,41 @@ import { useAPIModifier } from "../../../hooks/useAPIModifier.js";
 
 import { AuthenticationInfoContext } from "../../../App.jsx";
 import { LoadingMessage } from "../messages/LoadingMessage.jsx";
+import AddExampleForm from "./AddExampleForm.jsx";
 
 import "./form-styles.css";
 
 const EditWordForm = ({ targetCode }) => {
+    const authInfo = useContext(AuthenticationInfoContext)["authInfo"];
     const [wordData, setWordData] = useState({});
     const { apiFetch, loading, error } = useAPIFetcher();
+    const [initiallyExistingExamples, setInitiallyExistingExamples] = useState(
+        []
+    );
 
     useEffect(() => {
         apiFetch(
             `http://127.0.0.1:8000/api/korean_word/${targetCode}`,
+            authInfo["token"],
             setWordData
         );
     }, [targetCode]);
+
+    useEffect(() => {
+        if (
+            wordData &&
+            wordData["senses"] &&
+            wordData["senses"][0] &&
+            wordData["senses"][0]["additional_info"] &&
+            wordData["senses"][0]["additional_info"]["example_info"] &&
+            wordData["senses"][0]["additional_info"]["example_info"]
+        ) {
+            console.log("in");
+            setInitiallyExistingExamples(
+                wordData["senses"][0]["additional_info"]["example_info"]
+            );
+        }
+    }, [wordData]);
 
     return (
         <>
@@ -32,7 +54,10 @@ const EditWordForm = ({ targetCode }) => {
                         {"  [수정]"}
                     </span>
 
-                    <AddExampleForm wordTargetCode={targetCode} />
+                    <AddExampleForm
+                        wordTargetCode={targetCode}
+                        initiallyExistingExamples={initiallyExistingExamples}
+                    />
                     {/*
           <div className="senses-container">
             {formData["senses"] &&
@@ -48,107 +73,3 @@ const EditWordForm = ({ targetCode }) => {
 };
 
 export default EditWordForm;
-
-const AddExampleForm = ({ wordTargetCode, alreadyAtLeastOneExample }) => {
-    const authInfo = useContext(AuthenticationInfoContext)["authInfo"];
-    const [showForm, setShowForm] = useState(false);
-
-    const [exampleText, setExampleText] = useState("");
-    const [sourceText, setSourceText] = useState("");
-
-    const {
-        formData,
-        updateFormDataField,
-        apiModify,
-        successful,
-        response,
-        error,
-        loading,
-    } = useAPIModifier({
-        referent: wordTargetCode,
-        definition: "정의", // will not be rendered in sense view component because order is 0
-        additional_info: {
-            example_info: [
-                {
-                    source: "",
-                    example: "",
-                },
-            ],
-        },
-        order: 0,
-    });
-
-    const updateExampleInfo = (field, value) => {
-        const newFormData = { ...formData };
-        const newExampleInfo = [...newFormData.additional_info.example_info];
-        newExampleInfo[0] = { ...newExampleInfo[0], [field]: value };
-        newFormData.additional_info.example_info = newExampleInfo;
-        updateFormDataField("additional_info", newFormData.additional_info);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        apiModify(
-            "http://127.0.0.1:8000/api/create_sense/",
-            authInfo["token"],
-            formData,
-            "POST"
-        );
-    };
-
-    return (
-        <div className="add-example-form">
-            <div className="add-example-form-upper-bar">
-                <div className="add-example-header">예문 추가</div>
-            </div>
-
-            <div className="text-area-container">
-                <dl>
-                    <dt className="text-area-container-dt">예문 (필수)</dt>
-                    <dd className="text-area-container-dd">
-                        <textarea
-                            className="add-example-text-area"
-                            value={
-                                formData["additional_info"]["example_info"][0][
-                                    "example"
-                                ]
-                            }
-                            onChange={(event) =>
-                                updateExampleInfo("example", event.target.value)
-                            }
-                        ></textarea>
-                    </dd>
-                </dl>
-                <dl>
-                    <dt className="text-area-container-dt">출처 (선택)</dt>
-                    <dd className="text-area-container-dd">
-                        <textarea
-                            className="add-example-text-area"
-                            value={
-                                formData["additional_info"]["example_info"][0][
-                                    "source"
-                                ]
-                            }
-                            onChange={(event) =>
-                                updateExampleInfo("source", event.target.value)
-                            }
-                        ></textarea>
-                    </dd>
-                </dl>
-            </div>
-
-            <div className="add-example-form-lower-bar">
-                <div className="add-example-tip">
-                    예문 입력 시 중활고에 담는 단어 아래에 밑줄을 그려줍니다.
-                </div>
-                <button
-                    onClick={(e) => handleSubmit(e)}
-                    className="add-example-submit-button"
-                >
-                    추가
-                </button>
-            </div>
-        </div>
-    );
-};
