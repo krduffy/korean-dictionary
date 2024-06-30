d161cbfbbcdfab0693a286d7c1cf33545e77cb7d
yincade <101445104+yincade@users.noreply.github.com>
2024-06-30 17:39:05 -0400

    deleted some html in sense definitions

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-
 from .dictionary_models import KoreanWord, HanjaCharacter, Sense
 from .user_addition_models import UserNote
 
-admin.site.register(KoreanWord)
-admin.site.register(HanjaCharacter)
-admin.site.register(Sense)
-admin.site.register(UserNote)
+@admin.register(KoreanWord)
+class KoreanWordAdmin(admin.ModelAdmin):
+    search_fields = ('target_code', 'word')
+
+@admin.register(HanjaCharacter)
+class HanjaCharacterAdmin(admin.ModelAdmin):
+    search_fields = ('character',)
+
+@admin.register(Sense)
+class SenseAdmin(admin.ModelAdmin):
+    search_fields = ('target_code',)
+
+@admin.register(UserNote)
+class UserNoteAdmin(admin.ModelAdmin):
+    search_fields = ('id',)
---
+++ api/management/commands/remove_html.py
@@ -0,0 +1,63 @@
+from django.core.management.base import BaseCommand, no_translations
+from ...dictionary_models import Sense
+from django.db.models import Q
+from django.db import transaction
+import django
+import re
+
+django.setup()
+
+class Command(BaseCommand):
+
+  @no_translations
+  def handle(self, *args, **kwargs):
+    
+    
+    html_patterns = [
+        # outright deleted
+        '<I>', '</I>', '<sub>', '</sub>', '<sup>', '</sup>', '<IN>', '</IN>',
+        '<span class="korean-webfont">', '<span class="ngullim">', '</span>', '<FL>', '</FL>', 
+        '<strong>', '</strong>', '<i>', '</i>'
+    ]
+
+    q_filter = Q()
+    for pattern in html_patterns:
+        q_filter |= Q(definition__contains=pattern)
+
+    # not deleted outright; of the form <sense_no>\d*</sense_no> so needs to be subbed with re module
+    q_filter |= Q(definition__contains='<sense_no>')
+
+    senses_to_update = Sense.objects.filter(q_filter)
+
+    updated = 0
+    total = senses_to_update.count()
+    
+    self.stdout.write(f'Found {total} senses to process.')
+
+    batch_size = 1000
+    senses_to_save = []
+
+    with transaction.atomic():
+        for sense in senses_to_update:
+            original_definition = sense.definition
+            new_definition = original_definition
+
+            new_definition = re.sub(r'<sense_no>\d*</sense_no>', '', new_definition)
+
+            for pattern in html_patterns:
+                new_definition = new_definition.replace(pattern, '')
+
+            if new_definition != original_definition:
+                sense.definition = new_definition
+                senses_to_save.append(sense)
+                updated += 1
+
+            if len(senses_to_save) >= batch_size:
+                Sense.objects.bulk_update(senses_to_save, ['definition'])
+                senses_to_save = []
+                self.stdout.write(f'Updated {updated} senses so far.')
+
+        if senses_to_save:
+            Sense.objects.bulk_update(senses_to_save, ['definition'])
+
+    self.stdout.write(self.style.SUCCESS(f'Successfully finished executing command'))
